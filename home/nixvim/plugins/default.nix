2aecbb8cee6cd2bf0fcd7137366f4b1b1ecd773f
Ryan Glover <esatzryan@gmail.com>
2025-12-20 23:27:22 -0500

    Combine lsp-formatter and lspkind with lsp.nix

--- home/nixvim/plugins/default.nix
+++ home/nixvim/plugins/default.nix
@@ -5,8 +5,6 @@
     ./cmp.nix
     ./floaterm.nix
     ./lsp.nix
-    ./lsp-format.nix
-    ./lspkind.nix
     ./luasnip.nix
     ./telescope.nix
     ./treesitter.nix
--- home/nixvim/plugins/lsp.nix
+++ home/nixvim/plugins/lsp.nix
@@ -1,37 +1,54 @@
 { ... }:
 
 {
-  programs.nixvim.plugins.lsp = {
-    enable = true;
-    keymaps = {
-      silent = true;
-      diagnostic = {
-        "<leader>k" = "goto_prev";
-        "<leader>j" = "goto_next";
-      };
+  programs.nixvim.plugins = {
+    lsp-format.enable = true;
+    lsp = {
+      enable = true;
+      keymaps = {
+        silent = true;
+        diagnostic = {
+          "<leader>k" = "goto_prev";
+          "<leader>j" = "goto_next";
+        };
 
-      lspBuf = {
-        gd = "definition";
-        gD = "references";
-        gt = "type_definition";
-        gi = "implementation";
-        K = "hover";
-      };
-    };
-    servers = {
-      bashls.enable = true;
-      dockerls.enable = true;
-      nixd = {
-        enable = true;
-        settings = {
-          nixpkgs = {
-            expr = "import <nixpkgs> { }";
-          };
+        lspBuf = {
+          gd = "definition";
+          gD = "references";
+          gt = "type_definition";
+          gi = "implementation";
+          K = "hover";
         };
       };
-      postgres_lsp.enable = true;
-      ruby_lsp.enable = true;
-      terraformls.enable = true;
+      servers = {
+        bashls.enable = true;
+        dockerls.enable = true;
+        nixd = {
+          enable = true;
+          settings = {
+            nixpkgs = {
+              expr = "import <nixpkgs> { }";
+            };
+          };
+        };
+        ruby_lsp.enable = true;
+        terraformls.enable = true;
+      };
+    };
+
+    lspkind = {
+      enable = true;
+      settings.cmp = {
+        enable = true;
+        menu = {
+          nvim_lsp = "[LSP]";
+          nvim_lua = "[api]";
+          path = "[path]";
+          luasnip = "[snip]";
+          buffer = "[buffer]";
+          neorg = "[neorg]";
+        };
+      };
     };
   };
 }
